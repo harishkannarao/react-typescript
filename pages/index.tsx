7f99dcb26d4b37e7b367165ba13184e4ad32fe04
harishkannarao <harishkannarao@users.noreply.github.com>
2021-05-30 12:02:31 +0100

    Migrating all pages to functional components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
 import Head from 'next/head'
 import Link from 'next/link'
 
-export class HomePage extends React.Component {
-    render() {
-        return (
-            <div>
-                <Head>
-                    <title>Next.js!!!</title>
-                </Head>
-                <div data-testid="message">Welcome to Next.js!!!</div>
-                <h3>
-                    <Link href="/customers/list/">
-                        <a>Customers List</a>
-                    </Link>
-                </h3>
-            </div>
-        );
-    }
+export function HomePage() {
+    return (
+        <div>
+            <Head>
+                <title>Next.js!!!</title>
+            </Head>
+            <div data-testid="message">Welcome to Next.js!!!</div>
+            <h3>
+                <Link href="/customers/list/">
+                    <a>Customers List</a>
+                </Link>
+            </h3>
+        </div>
+    );
 }
 
-export default HomePage
+export default HomePage;
--- pages/sample/sample_page.tsx
+++ pages/sample/sample_page.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
 import Head from 'next/head'
 
-export class SamplePage extends React.Component {
-    render() {
-        return (
-            <div>
-                <Head>
-                    <title data-testid="title">Sample</title>
-                </Head>
-                <div data-testid="message">This is a sample page</div>
-            </div>          
-        );
-    }
+export function SamplePage() {
+    return (
+        <div>
+            <Head>
+                <title data-testid="title">Sample</title>
+            </Head>
+            <div data-testid="message">This is a sample page</div>
+        </div>
+    );
 }
 
-export default SamplePage  
+export default SamplePage;
